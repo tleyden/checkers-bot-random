a655297d6cb3d82c85de19b9a1378a46abe19901
Traun Leyden <traun@couchbase.com>
2013-10-03 16:24:57 -0700

    named return value felt obtuse here

--- randomthinker.go
+++ randomthinker.go
@@ -8,10 +8,9 @@ type RandomThinker struct {
 	ourTeamId int
 }
 
-func (r RandomThinker) Think(gameState cbot.GameState) (bestMove cbot.ValidMove) {
+func (r RandomThinker) Think(gameState cbot.GameState) cbot.ValidMove {
 	ourTeam := gameState.Teams[r.ourTeamId]
 	allValidMoves := ourTeam.AllValidMoves()
 	randomValidMoveIndex := cbot.RandomIntInRange(0, len(allValidMoves))
-	bestMove = allValidMoves[randomValidMoveIndex]
-	return
+	return allValidMoves[randomValidMoveIndex]
 }

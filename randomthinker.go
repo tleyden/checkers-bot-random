92a581fedd0d7bbc881ec3ee6afb1b28629cefbb
Traun Leyden <traun@couchbase.com>
2013-10-03 16:25:17 -0700

    Revert "named return value felt obtuse here"
    This reverts commit a655297d6cb3d82c85de19b9a1378a46abe19901.

--- randomthinker.go
+++ randomthinker.go
@@ -8,9 +8,10 @@ type RandomThinker struct {
 	ourTeamId int
 }
 
-func (r RandomThinker) Think(gameState cbot.GameState) cbot.ValidMove {
+func (r RandomThinker) Think(gameState cbot.GameState) (bestMove cbot.ValidMove) {
 	ourTeam := gameState.Teams[r.ourTeamId]
 	allValidMoves := ourTeam.AllValidMoves()
 	randomValidMoveIndex := cbot.RandomIntInRange(0, len(allValidMoves))
-	return allValidMoves[randomValidMoveIndex]
+	bestMove = allValidMoves[randomValidMoveIndex]
+	return
 }

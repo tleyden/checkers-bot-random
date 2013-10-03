609a044f249feee140a69647e34a345f9760926b
Traun Leyden <traun@couchbase.com>
2013-10-02 23:55:25 -0700

    pass in thinker

--- main/main.go
+++ main/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"github.com/couchbaselabs/logg"
 	cbot "github.com/tleyden/checkers-bot"
+	cbotrandom "github.com/tleyden/checkers-bot-random"
 )
 
 func main() {
 
 	logg.LogKeys["MAIN"] = true
 
+	randomThinker := new(cbotrandom.RandomThinker)
 	redTeam := cbot.RED_TEAM
-	game := cbot.NewGame(redTeam)
+	game := cbot.NewGame(redTeam, randomThinker)
 	game.GameLoop()
 
 }

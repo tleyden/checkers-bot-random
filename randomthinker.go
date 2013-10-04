f324ceec451b6b0c31fca4ae1aa8ff6ee2e5680c
Traun Leyden <traun@couchbase.com>
2013-10-03 19:23:48 -0700

    re-order funcs

--- randomthinker.go
+++ randomthinker.go
@@ -9,6 +9,14 @@ type RandomThinker struct {
 	ourTeamId int
 }
 
+func (r RandomThinker) Think(gameState cbot.GameState) (bestMove cbot.ValidMove) {
+	ourTeam := gameState.Teams[r.ourTeamId]
+	allValidMoves := ourTeam.AllValidMoves()
+	randomValidMoveIndex := cbot.RandomIntInRange(0, len(allValidMoves))
+	bestMove = allValidMoves[randomValidMoveIndex]
+	return
+}
+
 func init() {
 	logg.LogKeys["MAIN"] = true
 }
@@ -18,12 +26,4 @@ func main() {
 	thinker.ourTeamId = cbot.RED_TEAM
 	game := cbot.NewGame(cbot.RED_TEAM, thinker)
 	game.GameLoop()
-}
-
-func (r RandomThinker) Think(gameState cbot.GameState) (bestMove cbot.ValidMove) {
-	ourTeam := gameState.Teams[r.ourTeamId]
-	allValidMoves := ourTeam.AllValidMoves()
-	randomValidMoveIndex := cbot.RandomIntInRange(0, len(allValidMoves))
-	bestMove = allValidMoves[randomValidMoveIndex]
-	return
 }
